95c88eff46aadc98ccdebf425c2f09df094bf91a
Usuario <MacMini@Mac-mini-de-Camilo.local>
2025-01-06 00:07:09 -0300

    install getj

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,10 @@
 "use client";
-import { Select } from "@/components/ui";
+import { useState } from "react";
+import { IconRefresh } from "justd-icons";
+
+import { Select, Checkbox, Button, ProgressCircle } from "@/components/ui";
 import { RiMapPin2Line, RiCheckboxBlankCircleFill } from "react-icons/ri";
+
 export const centros = [
   { id: 1, name: "Becerra S23" },
   { id: 2, name: "Chaitén" },
@@ -9,10 +13,10 @@ export const centros = [
   { id: 5, name: "Santa Fe" },
   { id: 6, name: "Santa Rosa" },
 ];
- 
+
 export default function Dashboard() {
   return (
-    <main className="w-full grid grid-rows-4 text-white">
+    <main className="w-full grid grid-rows-4 text-white gap-5">
       <div className="grid grid-cols-6 gap-2 rounded-2xl  w-full bg-neutral-950/60 shadow shadow-neutral-700 overflow-hidden p-2">
         <article className="flex flex-col justify-start  shadow shadow-neutral-500 rounded-s-xl p-1">
           <div className="w-full grid grid-cols-2 bg-neutral-800/90 rounded-ss-lg">
@@ -31,7 +35,7 @@ export default function Dashboard() {
               CENTRO
             </h3>
             <div className=" flex flex-col gap-4 justify-center items-center">
-              <p className="bg-rose-600/90 text-rose-100 font-black px-4 py-1 rounded-sm shadow shadow-rose-400">
+              <p className="border border-rose-600 text-rose-100 font-black px-4 py-1 rounded-sm shadow shadow-rose-400">
                 BECERRA S23
               </p>
               <p className="flex items-center gap-1  text-xs">
@@ -50,11 +54,11 @@ export default function Dashboard() {
             </h2>
           </div>
           <div className="grid grid-cols-2 gap-2 h-full justify-center ">
-            <article className="bg-gradient-to-tr flex justify-center items-center from-blue-500 to-cyan-400 rounded">
-              <p className="font-black text-2xl text-neutral-200">M100</p>
+            <article className="border border-blue-500 flex justify-center items-center  rounded">
+              <p className="font-black text-2xl text-blue-500">M100</p>
             </article>
             <article className="flex flex-col justify-center items-center text-blue-500 font-semibold">
-              <button className="relative rounded-xl hover:bg-neutral-950 shadow shadow-blue-500 border hover:border-blue-400 py-2">
+              <button className="relative rounded-xl px-8 hover:bg-neutral-950 shadow shadow-blue-500 border border-blue-600 hover:border-blue-400 py-2">
                 Alertas
                 <span className="absolute  -top-2 -right-1 text-xs font-black border border-red-500   bg-red-500 px-1 rounded-lg text-white">
                   1
@@ -69,11 +73,11 @@ export default function Dashboard() {
               Cambiar Centro
             </h2>
           </div>
-          <div className="grid grid-cols-2 gap-2 h-full justify-center ">
+          <div className="grid grid-cols-2 gap-2 mt-5">
             <div>
-              <Select label="Design centros" placeholder="Select a centros">
+              <Select className="dark" placeholder="Select a centros">
                 <Select.Trigger />
-                <Select.List items={centros}>
+                <Select.List className="dark" items={centros}>
                   {(item) => (
                     <Select.Option id={item.id} textValue={item.name}>
                       {item.name}
@@ -83,10 +87,9 @@ export default function Dashboard() {
               </Select>
             </div>
             <div>
-              {" "}
-              <Select label="Design centros" placeholder="Select a centros">
+              <Select className="dark" placeholder="Select a centros">
                 <Select.Trigger />
-                <Select.List items={centros}>
+                <Select.List className="dark" items={centros}>
                   {(item) => (
                     <Select.Option id={item.id} textValue={item.name}>
                       {item.name}
@@ -94,11 +97,26 @@ export default function Dashboard() {
                   )}
                 </Select.List>
               </Select>
+            </div>
+            <div className=" flex justify-center items-center">
+              <Checkbox className={"dark"}>Seleccionar Centro</Checkbox>
+            </div>
+            <div className=" flex justify-center items-center">
+              <Button className="dark">
+                <IconRefresh />
+                Cambiar
+              </Button>
             </div>
           </div>
         </article>
         <article className="shadow shadow-neutral-500"></article>
         <article className="shadow shadow-neutral-500"></article>
+      </div>
+      <div className="grid grid-cols-4 gap-2 rounded-2xl  w-full bg-neutral-950/60 shadow shadow-neutral-700 overflow-hidden p-2">
+        <article>1</article>
+        <article>2</article>
+        <article>3</article>
+        <article>4</article>
       </div>
     </main>
   );
