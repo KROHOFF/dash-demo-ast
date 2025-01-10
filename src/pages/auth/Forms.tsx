4ae14e2b6f76bc5fbd960d9f168175ee27c01984
Usuario <MacMini@Mac-mini-de-Camilo.local>
2025-01-09 22:52:22 -0300

    Avances nav 2

--- src/pages/auth/Forms.tsx
+++ src/pages/auth/Forms.tsx
@@ -1,20 +1,34 @@
-"use client"
+"use client";
 
 import { Button, Form, TextField } from "@/components/ui";
 import { useNavigate } from "react-router";
 
 export function Forms() {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault(); 
-    navigate("/"); 
-  }
+    event.preventDefault();
+    navigate("/");
+  };
 
   return (
     <Form className="dark" onSubmit={handleSubmit}>
-      <TextField label="Correo Corporativo" type="email" isRequired className="mb-2" />
-      <TextField label="Contraseña" type="password" isRequired className="mb-2" />
-      <Button className="" type="submit">Acceder</Button>
+      <TextField
+        label="Correo Corporativo"
+        type="email"
+        isRequired
+        className="mb-2"
+      />
+      <TextField
+        label="Contraseña"
+        type="password"
+        isRequired
+        className="mb-2"
+      />
+      <div>
+        <Button className="w-full" type="submit">
+          Acceder
+        </Button>
+      </div>
     </Form>
-  )
+  );
 }
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,6 +1,7 @@
 import Background from "@/layouts/Background";
 import React from "react";
 import { Forms } from "./Forms";
+import { RiAppStoreLine, RiGooglePlayLine } from "react-icons/ri";
 
 export default function Login() {
   return (
@@ -19,7 +20,7 @@ export default function Login() {
               <div className="flex justify-center my-10 -mt-20">
                 <img className="h-14" src="logo.png" alt="Logo" />
               </div>
-              <p className="text-6xl font-bold text-neutral-200">
+              <p className="text-6xl font-bold text-neutral-600">
                 <span className="font-light text-red-500">WI</span>SENSOR
               </p>
               <p className="text-white text-center text-xl font-light">
@@ -29,11 +30,21 @@ export default function Login() {
           </div>
         </div>
         <div className="w-2/3 flex flex-col justify-center">
-          <p className="text-neutral-400 text-center text-xs font-light mb-5">
+          <p className="text-neutral-500 text-center text-xs font-light mb-5">
             Ingresa tus credenciales para continuar
           </p>
-          <div className="z-20 mx-auto w-[300px] bg-neutral-950/50 h-96 shadow shadow-neutral-600 rounded-2xl p-10">
+          <div className="z-20 mx-auto w-[300px] bg-neutral-950/50 shadow shadow-neutral-600 rounded-2xl p-10">
             <Forms />
+            <div className="mt-10 w-full flex flex-col gap-3 justify-center items-center">
+              <button className="flex justify-center items-center gap-2 border border-neutral-600 px-4 py-2 rounded-lg text-neutral-600 text-sm font-bold hover:text-blue-600 hover:border-blue-600 transition-all duration-300">
+                <RiAppStoreLine />
+                <span className="text-xs font-light ">Descargar</span>AppStore
+              </button>
+              <button className="flex justify-center items-center gap-2 border border-neutral-600 px-4 py-2 rounded-lg text-neutral-600 text-sm font-bold hover:text-red-500 hover:border-red-500 transition-all duration-300">
+                <RiGooglePlayLine />
+                <span className="text-xs font-light ">Descargar</span>PlayStore
+              </button>
+            </div>
           </div>
         </div>
       </main>
